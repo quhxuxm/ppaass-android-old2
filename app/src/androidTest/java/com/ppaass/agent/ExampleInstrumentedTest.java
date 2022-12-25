87bdbd464ff76552432d49aa5ce90c996bc75b5b
Qu Hao <quhxuxm_quh@163.com>
2022-12-25 18:04:03 +0800

    init

---
+++ app/src/androidTest/java/com/ppaass/agent/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.ppaass.agent;
+
+import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4.class)
+class ExampleInstrumentedTest {
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.ppaass.agent", appContext.getPackageName());
+    }
+}
--- app/src/test/java/com/ppaass/agent/EncryptionTest.java
+++ app/src/test/java/com/ppaass/agent/EncryptionTest.java
@@ -11,16 +11,16 @@ import java.io.IOException;
 public class EncryptionTest {
     @Test
     public void test() throws IOException {
-        byte[] publicKey = FileUtils.readFileToByteArray(
-                new File("D:\\Git\\ppaass-android\\app\\src\\main\\res\\raw\\proxypublickey"));
-        byte[] privateKey = FileUtils.readFileToByteArray(
-                new File("D:\\Git\\ppaass-android\\app\\src\\main\\res\\raw\\agentprivatekey"));
-        byte[] rsaEncrypted = FileUtils.readFileToByteArray(new File("D:\\rsa"));
-        CryptographyUtil.INSTANCE.init(publicKey, privateKey);
-        String aestoken=new String(CryptographyUtil.INSTANCE.rsaDecrypt(rsaEncrypted));
-        System.out.println(aestoken);
-        byte[] aesEncrypted = FileUtils.readFileToByteArray(new File("D:\\aes"));
-        String aesContent = new String(CryptographyUtil.INSTANCE.aesDecrypt(aestoken.getBytes(),aesEncrypted));
-        System.out.println(aesContent);
+//        byte[] publicKey = FileUtils.readFileToByteArray(
+//                new File("D:\\Git\\ppaass-android\\app\\src\\main\\res\\raw\\proxypublickey"));
+//        byte[] privateKey = FileUtils.readFileToByteArray(
+//                new File("D:\\Git\\ppaass-android\\app\\src\\main\\res\\raw\\agentprivatekey"));
+//        byte[] rsaEncrypted = FileUtils.readFileToByteArray(new File("D:\\rsa"));
+//        CryptographyUtil.INSTANCE.init(publicKey, privateKey);
+//        String aestoken=new String(CryptographyUtil.INSTANCE.rsaDecrypt(rsaEncrypted));
+//        System.out.println(aestoken);
+//        byte[] aesEncrypted = FileUtils.readFileToByteArray(new File("D:\\aes"));
+//        String aesContent = new String(CryptographyUtil.INSTANCE.aesDecrypt(aestoken.getBytes(),aesEncrypted));
+//        System.out.println(aesContent);
     }
 }
